683220122b0fea73a52e8e1306c8fe1c84ed2cd5
denis <denis.19.01@hotmail.com>
2025-01-29 00:00:43 -0500

    redirigir a pacientes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 use App\Http\Controllers\PacienteController;
 
 // Rutas del CRUD
+Route::get('/', function () {
+    return redirect()->route('pacientes.index');
+});
 Route::resource('pacientes', PacienteController::class);
 
 // Ruta para ver el historial de un paciente en particular
